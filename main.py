4a2b2c4d16c1458014e7015dd77a031da0525086
Sahil Ingle <inglesahil98@gmail.com>
2023-03-23 03:40:58 +0530

    comment added

--- main.py
+++ main.py
@@ -17,15 +17,17 @@ def hex2file(input_file, output_file):
 
 file2hex("github.png", "hex_data.txt")
 
+
+#delete all previous data from the file
 open('binary_data.txt', 'w').close()
 open('hex_data_opt.txt', 'w').close()
+
+#---------------converts hex data to binary data and store in the bin file-------------------------
 
 with open('hex_data.txt', 'r') as file:
     data = file.read().replace('\n', '')
 
-
 print("Initial string", data)
-
 
 # Code to convert hex to binary
 n = int(data, 16)
@@ -37,10 +39,12 @@ res = bStr
 
 with open("binary_data.txt", "r+") as binary_data:
     binary_data.write(res)
-
 # Print the resultant string
 print("Resultant string", str(res))
 
+
+
+#-------------------------------convert binary data to hex data and store in the hex_opt file-----------------------
 
 with open("binary_data.txt","r") as file:
     bnum = file.read()
@@ -52,6 +56,8 @@ with open("hex_data_opt.txt","w") as file:
     file.write(hdnum)
 print("\nEquivalent Hexadecimal Value = ", hdnum)
 
+
+#------------------------------------------------hex2file------------------------------------------------------------
 hex2file("hex_data_opt.txt","output.png")
 
 
